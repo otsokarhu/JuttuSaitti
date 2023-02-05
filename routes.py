f8c7219d266c9003e255c16a652fdaab80b32f77
Otso Westerlund <otsowesterlund@gmail.com>
2023-02-05 17:08:14 +0200

    added flask, templates for some of the base pages of the site

---
+++ routes.py
@@ -0,0 +1,32 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/add-post')
+def add_post():
+    return render_template('add_post.html')
